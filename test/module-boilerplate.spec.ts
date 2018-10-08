38096a6640a97dd7a2b774c3413862138db16b61
Andrew Faulkner <andfaulkner@gmail.com>
2018-10-08 01:54:16 -0400

    Strip tests down, make them work w ts 3.1.1 (and give them tsconfig)

--- test/module-boilerplate.spec.ts
+++ test/module-boilerplate.spec.ts
@@ -1,47 +1,16 @@
 /// <reference path="../node_modules/@types/mocha/index.d.ts" />
 /// <reference path="../node_modules/@types/node/index.d.ts" />
-
-// Ensure environment knows testing is occurring.
-process.env.mocha = true;
-
-// Store original process.argv.
-const oldProcArgs = Object.assign({}, process.argv);
+/// <reference path="../node_modules/typescript/lib/lib.es2018.full.d.ts" />
 
 /************************************** THIRD-PARTY IMPORTS ***************************************/
-import { expect } from 'chai';
-import * as sinon from 'sinon';
+import {expect} from 'chai';
+import sinon from 'sinon';
 
-import * as fs from 'fs';
-import * as path from 'path';
-import { stderr, stdout } from 'test-console';
+import fs from 'fs';
+import path from 'path';
 
 /*********************************** IMPORT FILES TO BE TESTED ************************************/
-const __nodeModuleBoilerplate__ = require('../lib/index');
-const nodeModuleBoilerplateExport = __nodeModuleBoilerplate__.nodeModuleBoilerplateExport;
-
-/******************************************** HELPERS *********************************************/
-/**
- * Prevents console.error messages emitted by code from reaching the console for given function
- * @param  {Function} fn - function to run without showing errors.
- * @return {Object<{errorLogs: string[], warnLogs: string[], result: any}>} array containing
- *              warnings & errors outputted running the function, and the function result.
- */
-function blockErrorOutput(fn) {
-    const errorLogs = [];
-    const warnLogs = [];
-
-    const errorOrig = console.error;
-    console.error = (...msgs) => errorLogs.push(msgs);
-    const warnOrig = console.warn;
-    console.warn = (...msgs) => warnLogs.push(msgs);
-
-    const result = fn();
-
-    console.error = errorOrig;
-    console.warn = warnOrig;
-
-    return { errorLogs, warnLogs, result };
-}
+import {nodeModuleBoilerplateExport} from '../index';
 
 /********************************************* TESTS **********************************************/
 describe('nodeModuleBoilerplateExport', function() {
@@ -53,12 +22,5 @@ describe('nodeModuleBoilerplateExport', function() {
     });
     it('#nodeModuleBoilerplateExport.nodeModuleBoilerplatePlaceholderFn throws if run', function() {
         expect(nodeModuleBoilerplateExport.nodeModuleBoilerplatePlaceholderFn).to.throw(Error);
-    });
-
-    /**
-     * Restore original process.argv.
-     */
-    after(function() {
-        process.argv = Object.assign({}, oldProcArgs);
     });
 });
